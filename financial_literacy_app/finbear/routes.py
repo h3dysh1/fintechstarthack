a53819af6a871fa76941e15ba901c425c6053a26
h3dysh1 <164467517+h3dysh1@users.noreply.github.com>
2024-07-08 01:17:30 +1000

    making the module into a package structure

--- financial_literacy_app/finbear/routes.py
+++ financial_literacy_app/finbear/routes.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect
+from finbear import app
+from finbear.forms import RegistrationForm, LoginForm
+from finbear.models import User
 from flask import request
 
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'KOWALA'
 
 # Sample data for lessons and quizzes
 lessons_data = [
@@ -32,9 +31,15 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/lessons')
@@ -68,6 +73,3 @@ def submit_quiz():
         score += 1
 
     return render_template('quiz_result.html', score=score)
-
-if __name__ == '__main__':
-    app.run(debug=True)
