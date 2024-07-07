b742e70ff733cb8bd729b12b460b64dbf8ff188e
h3dysh1 <164467517+h3dysh1@users.noreply.github.com>
2024-07-08 01:48:44 +1000

    adding login and user authentication

--- financial_literacy_app/finbear/routes.py
+++ financial_literacy_app/finbear/routes.py
@@ -1,8 +1,8 @@
-from flask import render_template, url_for, flash, redirect
-from finbear import app
+from flask import render_template, url_for, flash, redirect, request
+from finbear import app, db, bcrypt, login_manager
 from finbear.forms import RegistrationForm, LoginForm
 from finbear.models import User
-from flask import request
+from flask_login import login_user, current_user, login_required, logout_user
 
 
 # Sample data for lessons and quizzes
@@ -26,20 +26,31 @@ def home():
 @app.route('/register', methods=["GET", 'POST'])
 def register():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+            return redirect(url_for('home'))
     if form.validate_on_submit():
         flash(f'Account created for {form.username.data}!', 'success')
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/lessons')
@@ -73,3 +84,14 @@ def submit_quiz():
         score += 1
 
     return render_template('quiz_result.html', score=score)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
