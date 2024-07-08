543826f88b3a9224533ada8e75d0d0705aee6df8
h3dysh1 <164467517+h3dysh1@users.noreply.github.com>
2024-07-08 22:00:32 +1000

    hedy debugging from this base file

--- financial_literacy_app/finbear/models.py
+++ financial_literacy_app/finbear/models.py
@@ -1,13 +1,7 @@
-from finbear import db, login_manager
+from finbear import db
 from datetime import datetime
-from flask_login import UserMixin
 
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
