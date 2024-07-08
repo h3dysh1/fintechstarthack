4acb076fbc5536bd946d8c1bcd4ef160c9736d71
kistxe <kistxe@users.noreply.github.com>
2024-07-08 21:58:49 +1000

    login and quiz database table

--- financial_literacy_app/finbear/forms.py
+++ financial_literacy_app/finbear/forms.py
@@ -15,12 +15,12 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        user = User.get_or_none(username=username.data)
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
+        user = User.get_or_none(email=email.data)
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
 
--- financial_literacy_app/finbear/models.py
+++ financial_literacy_app/finbear/models.py
@@ -1,19 +1,31 @@
-from finbear import db, login_manager
-from datetime import datetime
+from finbear import app, db, login_manager
+from peewee import Model, CharField, DateTimeField, IntegerField, ForeignKeyField
+import datetime
 from flask_login import UserMixin
+
+# from sqlalchemy.orm import Mapped, mapped_column
+# from sqlalchemy import ForeignKey
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    return User.get_or_none(id = int(user_id))
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    '''posts = db.relationship('Post', backref='author', lazy=True)'''
+class BaseModel(Model):
+    class Meta:
+        database = db
 
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+class User(BaseModel, UserMixin):
+    username = CharField(max_length=20, unique=True)
+    email = CharField(max_length=50, unique=True)
+    password = CharField()
+
+class QuizScore(BaseModel):
+    score = IntegerField()
+    user = ForeignKeyField(User, backref='quiz_scores')
+    date = DateTimeField(default=datetime.datetime.now)
+
+db.connect()
+db.create_tables([User, QuizScore])
+
+
